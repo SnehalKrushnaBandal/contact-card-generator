9e94f390ffd5c35a036c9658caa4e9ff25925722
SnehalKrushnaBandal <skbandal02@gmail.com>
2025-07-03 15:48:56 +0530

    final commit

--- config/db_client.js
+++ config/db_client.js
@@ -1,4 +1,9 @@
 import {MongoClient} from "mongodb";
 import {env} from "./env.js";
 
-export const client = new MongoClient(env.MONGODB_URL);
+// export const client = new MongoClient(env.MONGODB_URL);
+export const client = new MongoClient(env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  tls: true
+});
